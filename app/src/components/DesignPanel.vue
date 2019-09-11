5c0999395e61d9782f663d1c9b1f68c4991dd0ab
Matías Bahamonde <mbahamonde2016@alu.uct.cl>
2019-09-11 12:35:33 -0300

    Implementacion jsMind con vue
    Primera parte prueba de implementacion libreria para mapas conceptuales

--- app/src/components/DesignPanel.vue
+++ app/src/components/DesignPanel.vue
@@ -1,7 +1,7 @@
 <template>
 <b-col md="9">
-    <div class="border h-100">
-        <h1>{{ arq }}</h1>
+    <div id="jsmind_container">
+        
     </div>
 </b-col>
 </template>
@@ -9,11 +9,57 @@
 <script>
 
 import { mapState } from 'vuex'
+import jsMind from 'jsmind'
 
 export default {
   name: 'DesignPanel',
-  computed: {
+  mounted: function () {
+    this.load_jsmind()
+  },
+  methods: {
+  load_jsmind: function(){
+    var mind = {
+        "meta":{
+            "name":"demo",
+            "author":"hizzgdev@163.com",
+            "version":"0.2",
+        },
+        "format":"node_array",
+        "data":[
+            {"id":"root", "isroot":true, "topic":"jsMind"},
+            {"id":"sub1", "parentid":"root", "topic":"sub1", "background-color":"#0000ff"},
+            {"id":"sub11", "parentid":"sub1", "topic":"sub11"},
+            {"id":"sub12", "parentid":"sub1", "topic":"sub12"},
+            {"id":"sub13", "parentid":"sub1", "topic":"sub13"},
+            {"id":"sub2", "parentid":"root", "topic":"sub2"},
+            {"id":"sub21", "parentid":"sub2", "topic":"sub21"},
+            {"id":"sub22", "parentid":"sub2", "topic":"sub22","foreground-color":"#33ff33"},
+            {"id":"sub3", "parentid":"root", "topic":"sub3"},
+        ]
+    };
+    var options = {
+        container:'jsmind_container',
+        editable:true,
+        theme:'primary'
+    }
+    var jm = jsMind.show(options,mind);
+    // jm.set_readonly(true);
+    // var mind_data = jm.get_data();
+    // alert(mind_data);
+    jm.add_node("sub2","sub23", "new node", {"background-color":"red"});
+    jm.set_node_color('sub21', 'green', '#ccc');
+    }
+  },computed: {
     ...mapState(['arq'])
   }
 }
 </script>
+<style>
+  #jsmind_container{
+    width:800px;
+    height:500px;
+    border:solid 1px #ccc;
+    /*background:#f4f4f4;*/
+    background:#f4f4f4;
+  }
+</style>
--- app/src/main.js
+++ app/src/main.js
@@ -8,6 +8,7 @@ Vue.use(BootstrapVue)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'jsmind/style/jsmind.css'
 
 Vue.config.productionTip = false
 
